3e81e3e88c6dc435f1de3735bb9a12e3141d6ef2
Vahid Mavaji <mavaji@outlook.com>
2018-11-04 10:59:57 +0330

    add lesson3

---
+++ lesson3/fig.3.4.hs
@@ -0,0 +1,16 @@
+main :: IO()
+main = do
+  print("add1 1 = " ++ show(add1 1))
+  print("add2 1 = " ++ show(add2 1))
+  print("add3 1 = " ++ show (add3 1))
+
+x = 4
+
+add1 :: Int -> Int
+add1 y = y + x
+
+add2 :: Int -> Int
+add2 y = (\x -> y + x) 3
+
+add3 :: Int -> Int
+add3 y = (\y -> (\x -> y + x) 1) 2
