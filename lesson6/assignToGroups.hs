247b142b75e7bdd7e06f6824582626551349a02b
Vahid Mavaji <mavaji@outlook.com>
2018-11-08 12:34:43 +0330

    listing 6.6

---
+++ lesson6/assignToGroups.hs
@@ -0,0 +1,10 @@
+main :: IO()
+main = do
+    print (assignToGroups 3 ["fi1e1.txt", "file2.txt", "file3.txt",
+                            "file4.txt", "file5.txt", "file6.txt",
+                            "file7.txt", "file8.txt"])
+
+    print (assignToGroups 2 ["Bob", "Kathy", "Sue", "Joan", "Jim", "Mike"])
+
+assignToGroups n aList = zip groups aList
+    where groups = cycle [1..n]     
