f2609fb90a65a1a17b1d3c9692bae71732c0a535
Vahid Mavaji <mavaji@outlook.com>
2018-11-08 11:14:15 +0330

    listing5.5

---
+++ lesson5/myExampleUrlBuilder.hs
@@ -0,0 +1,23 @@
+main :: IO()
+main = do
+    print (getRequestUrl "http://example.com" "1337hAsk3ll" "book" "1234")
+    print (exampleUrlBuilder "1337hAsk3ll" "book" "1234")
+    print (myExampleUrlBuilder "book" "1234")
+
+myExampleUrlBuilder = genApiRequestBuilder exampleUrlBuilder "1337hAsk3ll"
+
+genApiRequestBuilder hostBuilder apiKey = (\resource id ->
+                                            hostBuilder apiKey resource id)
+
+exampleUrlBuilder = genHostRequestBuilder "http://example.com"
+
+genHostRequestBuilder host = (\apiKey resource id ->
+                               getRequestUrl host apiKey resource id) 
+
+getRequestUrl host apiKey resource id = host ++
+                                        "/" ++
+                                        resource ++
+                                        "/" ++
+                                        id ++
+                                        "?token=" ++
+                                        apiKey
