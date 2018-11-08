62eec0cc11c87ed41bc8d0c2dff5b9ba0303a3e5
Vahid Mavaji <mavaji@outlook.com>
2018-11-08 13:10:24 +0330

    lesson7

---
+++ lesson7/myGCD.hs
@@ -0,0 +1,8 @@
+main :: IO()
+main = do
+    print (myGCD 20 16)
+
+myGCD a b = if remainder == 0
+            then b
+            else myGCD b remainder
+    where remainder = a `mod` b            
