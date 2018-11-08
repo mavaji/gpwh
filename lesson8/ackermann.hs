f276af494f85732b7d843eacd2594f2a78381f64
Vahid Mavaji <mavaji@outlook.com>
2018-11-08 19:00:39 +0330

    ackermann collatz

---
+++ lesson8/ackermann.hs
@@ -0,0 +1,9 @@
+main :: IO()
+main = do
+    print (ackermann 3 3)
+    print (ackermann 3 8)
+    print (ackermann 3 9)
+
+ackermann 0 n = n + 1
+ackermann m 0 = ackermann (m-1) 1
+ackermann m n = ackermann (m-1) (ackermann m (n-1))
---
+++ lesson8/collatz.hs
@@ -0,0 +1,10 @@
+main :: IO()
+main = do
+    print (collatz 9)
+    print (collatz 999)
+    print (collatz 92)
+    print (collatz 91)
+    print (map collatz [100..120])
+
+collatz 1 = 1
+collatz n = if even n then 1 + collatz (n `div` 2) else 1 + collatz (n*3 + 1)
