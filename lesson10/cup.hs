df767a9a20dbf49c0340dd1b30cc3e0dd5d25a61
Vahid Mavaji <mavaji@outlook.com>
2018-11-09 17:53:03 +0330

    lesson 10

---
+++ lesson10/cup.hs
@@ -0,0 +1,28 @@
+main :: IO ()
+main = do
+    print (getOz coffeeCup)
+    print (getOz afterASip)
+    print (getOz afterTwoSips)
+    print (getOz afterGulp)
+    print (getOz afterBigGulp)
+    print (getOz afterManySips)
+
+cup flOz = \message -> message flOz
+
+coffeeCup = cup 12
+
+getOz aCup = aCup (\flOz -> flOz)
+
+drink aCup ozDrank = if ozDiff >= 0
+                     then cup ozDiff
+                     else cup 0
+    where flOz = getOz aCup
+          ozDiff = flOz - ozDrank
+
+isEmpty aCup = getOz aCup == 0
+
+afterManySips = foldl drink coffeeCup [1,1,1,1,1]
+afterBigGulp = drink coffeeCup 20
+afterASip = drink coffeeCup 1
+afterTwoSips = drink afterASip 1
+afterGulp = drink afterTwoSips 4
