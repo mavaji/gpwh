0877a8bce29c832cc4941edd9da417743faf12f7
Vahid Mavaji <mavaji@outlook.com>
2018-11-18 01:20:02 +0330

    listing 17.12

---
+++ unit3/lesson17/listing-17.12.hs
@@ -0,0 +1,19 @@
+main :: IO()
+main = do
+    print (createPTable ["heads", "tails"] [0.5, 0.5])
+
+type Events = [String]
+type Probs = [Double]
+
+data PTable = PTable Events Probs
+
+createPTable :: Events -> Probs -> PTable
+createPTable events probs = PTable events normalizedProbs
+    where totalProbs = sum probs
+          normalizedProbs = map (\x -> x/totalProbs) probs
+
+showPair :: String -> Double -> String
+showPair event prob = mconcat [event,"|",show prob, "\n"]
+
+instance Show PTable where
+    show (PTable events probs) = mconcat pairs where pairs = zipWith showPair events probs
