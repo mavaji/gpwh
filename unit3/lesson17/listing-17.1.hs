341b040b60bd5be29f04ffc316365fd4244495c6
Vahid Mavaji <mavaji@outlook.com>
2018-11-18 00:34:19 +0330

    unit3

---
+++ unit3/lesson17/listing-17.1.hs
@@ -0,0 +1,27 @@
+import Data.List
+
+main :: IO()
+main = do
+    print(myLast [1,2,3])
+    print(myMin [8,5,10,13])
+    print(myMax [8,5,10,13])
+
+    print(myAll (\x -> even x) [2,4,6,8])
+    print(myAll (\x -> even x) [2,4,6,7])
+
+    print(myAny (\x -> odd x) [2,4,6,7])
+    
+myLast :: [a] -> a
+myLast = head.reverse
+
+myMin :: Ord a => [a] -> a
+myMin = head.sort
+
+myMax :: Ord a => [a] -> a
+myMax = myLast.sort
+
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll testFunc = (foldr (&&) True).(map testFunc)
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny testFunc = (foldr (||) False).(map testFunc)
