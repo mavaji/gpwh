c3624393110f9a1ee3e066850702ed741f5ede1b
Vahid Mavaji <mavaji@outlook.com>
2018-12-04 15:35:01 +0330

    unit 5

---
+++ unit5/listing-27.2.hs
@@ -0,0 +1,14 @@
+main :: IO ()
+main = do
+    print(incMaybe successfulRequest)
+    print(incMaybe failedRequest)
+
+incMaybe :: Maybe Int -> Maybe Int
+incMaybe (Just n) = Just (n + 1)
+incMaybe Nothing = Nothing
+
+successfulRequest :: Maybe Int
+successfulRequest = Just 6
+
+failedRequest :: Maybe Int
+failedRequest = Nothing
