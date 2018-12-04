5fb8be24ee76c4a7a7c28e8ca00c2a2d33f6812b
Vahid Mavaji <mavaji@outlook.com>
2018-12-04 18:04:51 +0330

    lesson 19

---
+++ unit3/lesson19/listing-19.5.hs
@@ -0,0 +1,33 @@
+import qualified Data.Map as Map
+
+main :: IO ()
+main = do
+    print (countOrgan Brain availableOrgans)
+    print (countOrgan Heart availableOrgans)
+
+countOrgan :: Organ -> [Maybe Organ] -> Int
+countOrgan organ available = length (filter (\x -> x == Just organ) available)
+
+availableOrgans :: [Maybe Organ]
+availableOrgans = getDrawerContents possibleDrawers organCatalog
+
+getDrawerContents :: [Int] -> Map.Map Int Organ -> [Maybe Organ]
+getDrawerContents ids catalog = map getContents ids
+    where getContents = \id -> Map.lookup id catalog
+
+possibleDrawers :: [Int]
+possibleDrawers = [1 .. 50]
+
+data Organ = Heart | Brain | Kidney | Spleen deriving (Show, Eq)
+
+organs :: [Organ]
+organs = [Heart, Heart, Brain, Spleen, Spleen, Kidney]
+
+ids :: [Int]
+ids = [2, 7, 13, 14, 21, 24]
+
+organPairs :: [(Int, Organ)]
+organPairs = zip ids organs
+
+organCatalog :: Map.Map Int Organ
+organCatalog = Map.fromList organPairs
