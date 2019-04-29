b89df3cc092e3a315ab6a8061ec58ff7264f22f1
Vahid Mavaji <mavaji@outlook.com>
2019-04-29 17:02:26 +0430

    Code 30_14

--- src/Unit5/Lesson30/Code30_14.hs
+++ src/Unit5/Lesson30/Code30_14.hs
@@ -2,6 +2,7 @@ module Unit5.Lesson30.Code30_14 where
 
 main30_14 :: IO ()
 main30_14 = askForName >> getLine >>= (\name -> return (nameStatement name)) >>= putStrLn
+--main30_14 = (nameStatement <$> (askForName >> getLine)) >>= putStrLn
 
 nameStatement :: String -> String
 nameStatement name = "Hello, " ++ name ++ "!"
